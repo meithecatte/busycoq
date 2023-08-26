250b26cca8faa6a42e98e909d2d01a5da868d9b2
Maja Kądziołka <maya@compilercrim.es>
2023-08-26 09:13:24 +0200

    Individual.v: document the tactics

--- verify/Individual.v
+++ verify/Individual.v
@@ -17,26 +17,53 @@ Lemma move_right_const : forall l s s0,
   move_right (l {{s}} const s0) = l << s {{s0}} const s0.
 Proof. reflexivity. Qed.
 
+(** The direct formulation isn't as useful when the proof that the two
+    configurations are the same is non-trivial. *)
 Lemma evstep_refl' : forall tm c c',
   c = c' ->
   c -[ tm ]->* c'.
 Proof. intros. subst c'. auto. Qed.
 
+(** Solve an equality goal where some subexpressions are equal by [lia],
+    in an otherwise [reflexivity]-compatible spine. *)
 Ltac lia_refl := solve [repeat (lia || f_equal)].
 
+(** [prove_step] proves a goal of the form [c -[ tm ]-> ?c'], where the value
+    returned by [tm] in this situation can be calculated by reflexivity. *)
 Ltac prove_step_left := apply @step_left; reflexivity.
 Ltac prove_step_right := apply @step_right; reflexivity.
 Ltac prove_step := prove_step_left || prove_step_right.
+
+(** Simplify a tape expression, removing [move_left] and [move_right] leftover
+    after [prove_step], without needlessly expanding the [cofix] in [const s0]. *)
 Ltac simpl_tape :=
   try rewrite move_left_const;
   try rewrite move_right_const;
   simpl;
   try rewrite <- const_unfold.
+
+(** Prove a goal of the form [c -->+ c'] that consists of a single TM step. *)
 Ltac finish_progress := apply progress_base; prove_step.
+
+(** Prove a goal of the form [c -->* c'] that consists of zero TM steps. *)
 Ltac finish_evstep := apply evstep_refl'; try (reflexivity || lia_refl).
 Ltac finish := finish_evstep || finish_progress.
+
+(** Advance the configuration on the left-hand side of a [-->+] or [-->*]
+    by one TM step. *)
 Ltac step := (eapply evstep_step || eapply progress_step); [prove_step | simpl_tape].
+
+(** Run [step] until we reach the state that is being asked for, or until the
+    TM gets stuck (because the symbolic state doesn't make it clear what symbol
+    is under the tape). *)
 Ltac execute := introv; repeat (try solve [finish]; step).
+
+(** [follow H], on a goal of the form [H: c1 -->* c2  |-  c1' -->* c3], will
+    transform it into [|-  c2 -->* c3].  [adjust] is used to make it work when
+    the equality [c1 = c1'] isn't as clear.
+
+    [follow], without an argument, will try using the assumptions
+    in the context. *)
 Ltac do_adjust H ty :=
   lazymatch ty with
   | _ -> ?ty => do_adjust H ty
@@ -58,6 +85,7 @@ Ltac follow_assm :=
 Tactic Notation "follow" := follow_assm.
 Tactic Notation "follow" constr(H) := follow_hyp H.
 
+(** For trivial [-->*] goals, provable by stepping and applying assumptions. *)
 Ltac triv := intros; repeat (try solve [finish]; (step || follow)).
 
 Notation "l <{{ q }} r" := (q;; tl l {{hd l}} r)  (at level 30, q at next level).
