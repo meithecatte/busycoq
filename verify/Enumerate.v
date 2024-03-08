109f5fb061f3e46e49687fce5870ac9b8a825862
Maja Kądziołka <maya@compilercrim.es>
2024-03-08 21:32:14 +0100

    A bit more WIP
    this is what i do when i switch between the desktop and laptop.
    "use a branch" i will never do such a thing, you heathen!

--- verify/Enumerate.v
+++ verify/Enumerate.v
@@ -1,20 +1,65 @@
 (** * Enumerate: TNF enumeration *)
 
+From Coq Require Import List. Import ListNotations.
 From BusyCoq Require Export Permute.
 Set Default Goal Selector "!".
 
 Module Enumerate (Ctx : Ctx).
   Module Permute := Permute Ctx. Export Permute.
 
-Inductive ReachesNZ (tm : TM) : Q -> nat -> Prop :=
-  | Reaches_halt q :
+(** To justify only enumerating machines that start with 1RB, we need
+    to prove that all the other machines won't take many steps to write
+    their first 1 (or a non-zero symbol in the case of non-binary Turing
+    machines). *)
+
+(** [tm] visits the sequence of states [q :: qs] before writing its first
+    non-zero symbol. *)
+Inductive zvisits (tm : TM) : Q -> list Q -> Prop :=
+  | zvisits_halt q :
     tm (q, s0) = None ->
-    ReachesNZ tm q 0
-  | Reaches_now q s d q' :
+    zvisits tm q []
+  | zvisits_nz q s d q' :
     tm (q, s0) = Some (s, d, q') ->
     s <> s0 ->
-    ReachesNZ tm q 0
-  | Reaches_later q d q' n :
+    zvisits tm q []
+  | zvisits_step q d q' qs :
     tm (q, s0) = Some (s0, d, q') ->
-    ReachesNZ tm q' n ->
-    ReachesNZ tm q (S n).
+    zvisits tm q' qs ->
+    zvisits tm q (q' :: qs).
+
+(** This sequence is unique. *)
+
+Lemma zvisits_unique : forall tm qs qs' q,
+  zvisits tm q qs ->
+  zvisits tm q qs' ->
+  qs = qs'.
+Proof.
+  induction qs as [| a qs IH]; introv H1 H2;
+    inverts H1; inverts H2; try congruence.
+  assert (a = q'). { congruence. }
+  subst. f_equal. eauto.
+Qed.
+
+(** Therefore, [q] cannot occur in [qs]. *)
+Lemma zvisits_In_skip : forall tm qs q q',
+  zvisits tm q qs ->
+  In q' qs ->
+  exists qs', zvisits tm q' qs' /\ qs <> qs'.
+Proof.
+  induction qs as [| a qs IH].
+  - contradiction.
+  - introv Hvisits Hin. inverts Hvisits as Etm Hvisits.
+    destruct Hin as [E | Hin]; subst; eauto.
+Qed.
+
+Lemma zvisits_In : forall tm qs q,
+  zvisits tm q qs ->
+  ~ In q qs.
+Proof.
+  introv H1 Hin.
+  apply 
+  induction qs as [| a qs IH].
+  - contradiction.
+  - inverts H1 as Etm H1.
+    * subst a.
+  assert (H2: zvisits tm 
---
+++ verify/Pigeonhole.v
@@ -0,0 +1,74 @@
+From Coq Require Import List. Import ListNotations.
+From Coq Require Import Lia.
+From BusyCoq Require Export LibTactics.
+Set Default Goal Selector "!".
+
+(** [repeats xs] if [xs] repeats an element. *)
+Inductive repeats {X : Type} : list X -> Prop :=
+  | repeats_head x xs : In x xs -> repeats (x :: xs)
+  | repeats_cons x xs : repeats xs -> repeats (x :: xs).
+
+Hint Constructors repeats : core.
+
+Lemma in_split : forall (X : Type) (x : X) (l : list X),
+  In x l ->
+  exists l1 l2, l = l1 ++ x :: l2.
+Proof.
+  introv H. induction l as [| a l IH].
+  - inverts H.
+  - destruct H.
+    + subst a. exists (@nil X), l. reflexivity.
+    + specialize (IH H). destruct IH as [l1' [l2' IH]].
+      subst l. exists (a :: l1'), l2'. reflexivity.
+Qed.
+
+Hint Extern 1 (In _ (_ :: _)) => left : core.
+Hint Extern 1 (In _ (_ :: _)) => right : core.
+
+(* See below for the motivation on this lemma. *)
+Lemma insplit_decide : forall (X : Type) (x : X) (l1 l2a l2b : list X),
+  Forall (fun y => In y (l2a ++ x :: l2b)) l1 ->
+  In x l1 \/ Forall (fun y => In y (l2a ++ l2b)) l1.
+Proof.
+  introv Hsubset.
+  induction l1 as [| a l1 IH].
+  - auto.
+  - inverts Hsubset as Ha Hsubset. specialize (IH Hsubset).
+    (* Take care of the simple case where IH has already found x in l1. *)
+    destruct IH as [IH | IH]. { auto. }
+    apply in_app_or in Ha.
+    destruct Ha as [Ha | [Ha | Ha]]; auto using in_or_app.
+Qed.
+
+Theorem pigeonhole_principle : forall (X : Type) (l1 l2 : list X),
+  Forall (fun x => In x l2) l1 ->
+  length l2 < length l1 ->
+  repeats l1.
+Proof.
+  intros X l1. induction l1 as [| x l1 IH]; introv Hsubset Hlonger.
+  - inverts Hlonger.
+  - (* Proof sketch: if [In x l1], we win. Otherwise, remove x from l2,
+       which lets us preserve [Hlonger] for the induction hypothesis.
+
+       How do we decide if [In x l1], though?
+
+       Answer: [Hsubset] implicitly knows, for each element of [l1], where
+       it occurs in [l2]. Well, at least one occurrence of each. *)
+
+    (* First, we locate at least one occurrence of [x] in [l2]: *)
+    inverts Hsubset as Hx Hsubset.
+
+    (* This is the occurrence we'll be trying to remove for our induction step: *)
+    apply in_split in Hx.
+    destruct Hx as [l2a [l2b El2]]. subst l2.
+
+    (* Iterate over [l1] and see if [Hsubset] ever points at that [x]. If so,
+       [In x l1]. Otherwise we have the precondition of [IH]. *)
+    apply insplit_decide in Hsubset.
+    destruct Hsubset as [Hx | Hsubset]. { auto. }
+    specialize (IH _ Hsubset).
+
+    autorewrite with list in Hlonger, IH. simpl length in Hlonger.
+    apply repeats_cons, IH.
+    lia.
+Qed.
