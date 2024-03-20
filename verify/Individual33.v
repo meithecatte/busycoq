5d24eb41d48dfa2fd17ba529988f72f44f0117dd
Maja Kądziołka <maya@compilercrim.es>
2024-03-20 17:58:31 +0100

    Individual33: replicate the infrastructure of Individual52

--- verify/Individual33.v
+++ verify/Individual33.v
@@ -1,7 +1,23 @@
 From BusyCoq Require Export Individual BB33.
 
 Module Individual33 := Individual BB33.
+Export Individual33.
 
-Notation "0" := S0.
-Notation "1" := S1.
-Notation "2" := S2.
+Declare Scope sym_scope.
+Bind Scope sym_scope with Sym.
+Delimit Scope sym_scope with sym.
+Open Scope sym.
+
+Notation "0" := S0 : sym_scope.
+Notation "1" := S1 : sym_scope.
+Notation "2" := S2 : sym_scope.
+
+(* Make sure that [{{A}}>] still refers to the state, even if we shadowed
+   [A] itself with something else. *)
+Notation "l '{{A}}>'  r" := (l {{A}}> r) (at level 30).
+Notation "l '{{B}}>'  r" := (l {{B}}> r) (at level 30).
+Notation "l '{{C}}>'  r" := (l {{C}}> r) (at level 30).
+
+Notation "l '<{{A}}' r" := (l <{{A}} r) (at level 30).
+Notation "l '<{{B}}' r" := (l <{{B}} r) (at level 30).
+Notation "l '<{{C}}' r" := (l <{{C}} r) (at level 30).
